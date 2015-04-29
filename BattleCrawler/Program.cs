cd987ce97484b60e2ca72bc59a9b2157369591da
Lisu <tennlos@gmail.com>
2015-04-29 10:33:11 +0200

    local commit

--- BattleCrawler/Program.cs
+++ BattleCrawler/Program.cs
@@ -7,21 +7,8 @@ namespace BattleCrawler
         public static void Main(string[] args)
         {
             var session = NHibernateHelper.GetCurrentSession();
-            Leader Marcin = new Leader();
-            Marcin.Name = "marcin";
-             
-            Belligerent Turcja = new Belligerent();
-            Turcja.Name = "turcja";
-            Marcin.Belligerent = Turcja;
-            Battle newBattle = new Battle();
-            newBattle.Name = "BitwaOWieden";
-            newBattle.Leaders = new List<Leader>();
-            newBattle.Leaders.Add(Marcin);
-            BattlesBelligerents battleBel = new BattlesBelligerents();
-            battleBel.Belligerent = Turcja;
-            battleBel.Battle = newBattle;
-            newBattle.Belligerents.Add(battleBel);
-            session.SaveOrUpdate(newBattle);
+            Crawler crawler = new Crawler(session);
+            crawler.Crawl();
         }
     }
 }
